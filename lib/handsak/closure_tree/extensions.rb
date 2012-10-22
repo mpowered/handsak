5e051d54a08ce5b64605a7f284c76123a1b6e00d
Gary Greyling <greyling.gary@gmail.com>
2012-10-22 12:06:22 +0200

    fix closure_tree extensions. Didn't work for middle tier linkages

--- lib/handsak/closure_tree/extensions.rb
+++ lib/handsak/closure_tree/extensions.rb
@@ -9,13 +9,17 @@ module Handsak
       def self.included(klass)
         # Returns the most senior nodes (nodes in the oldest generation)
         def klass.most_senior
-          most_senior_generation = joins(:ancestor_hierarchies).minimum("#{quoted_hierarchy_table_name}.generations")
+          most_senior_generation = joins(:ancestor_hierarchies)
+                                    .where("#{quoted_hierarchy_table_name}.ancestor_id <> #{quoted_hierarchy_table_name}.descendant_id")
+                                    .minimum("#{quoted_hierarchy_table_name}.generations")
           find_all_by_generation(most_senior_generation)
         end
 
         # Returns the most junior nodes (nodes in the youngest generation)
         def klass.most_junior
-          most_junior_generation = joins(:ancestor_hierarchies).maximum("#{quoted_hierarchy_table_name}.generations")
+          most_junior_generation = joins(:ancestor_hierarchies)
+                                    .where("#{quoted_hierarchy_table_name}.ancestor_id <> #{quoted_hierarchy_table_name}.descendant_id")
+                                    .maximum("#{quoted_hierarchy_table_name}.generations")
           find_all_by_generation(most_junior_generation)
         end
       end
