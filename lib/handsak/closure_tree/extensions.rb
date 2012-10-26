0819a0b2a1c22f69a278fc5518568fcd32207a0c
Gary Greyling <greyling.gary@gmail.com>
2012-10-26 12:12:42 +0200

    fix #most_senior and #most_junior... what was i thinking??

--- lib/handsak/closure_tree/extensions.rb
+++ lib/handsak/closure_tree/extensions.rb
@@ -10,16 +10,20 @@ module Handsak
         # Returns the most senior nodes (nodes in the oldest generation)
         def klass.most_senior
           most_senior_generation = joins(:ancestor_hierarchies)
-                                    .where("#{quoted_hierarchy_table_name}.ancestor_id <> #{quoted_hierarchy_table_name}.descendant_id")
-                                    .minimum("#{quoted_hierarchy_table_name}.generations")
+                                   .group(:descendant_id)
+                                   .maximum("#{quoted_hierarchy_table_name}.generations")
+                                   .values
+                                   .min
           find_all_by_generation(most_senior_generation)
         end
 
         # Returns the most junior nodes (nodes in the youngest generation)
         def klass.most_junior
           most_junior_generation = joins(:ancestor_hierarchies)
-                                    .where("#{quoted_hierarchy_table_name}.ancestor_id <> #{quoted_hierarchy_table_name}.descendant_id")
-                                    .maximum("#{quoted_hierarchy_table_name}.generations")
+                                   .group(:descendant_id)
+                                   .maximum("#{quoted_hierarchy_table_name}.generations")
+                                   .values
+                                   .max
           find_all_by_generation(most_junior_generation)
         end
       end
