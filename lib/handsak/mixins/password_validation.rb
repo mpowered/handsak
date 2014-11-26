c83c773eca605155a4b9d35676d0559e98639b01
Luke Randall <luke.randall@gmail.com>
2014-11-26 12:56:40 +0200

    Bypass unneeded multiline validation

--- lib/handsak/mixins/password_validation.rb
+++ lib/handsak/mixins/password_validation.rb
@@ -5,6 +5,7 @@ module Handsak
         klass.class_eval do
           validates_format_of :password,
             with: /\A(?=.*[^a-zA-Z])(?=.*[a-z])(?=.*[A-Z])\S{8,}\z/,
+            multiline: true,
             message: 'must be at least 8 characters long with at least one '\
                      'uppercase letter, one lowercase letter and one number',
             allow_nil: true # Authlogic will handle nil passwords
